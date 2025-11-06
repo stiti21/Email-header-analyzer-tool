a8779d0e448b65076984f55c0c2dddc1705d27dd
Solaf Altiti <solafaltiti21@gmail.com>
2025-11-06 16:19:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ import os
 import email          
 from email import policy   
 import csv             
+import mailbox
 
-directory_path = "/home/stiti/tool/dataset/emails"  # <-- Change if needed
+directory_path = "/home/stiti/tool/dataset/emails"  
 
 headers_to_extract = ["From", "To", "Cc", "Date", "Message-ID", "Return-Path" ,"Authentication-Results"]
 
-csv_file_path = "/home/stiti/tool/csv/email_headers.csv"  # Output CSV location
+csv_file_path = "/home/stiti/tool/csv/email_headers.csv"
 
 with open(csv_file_path, mode="w", newline="", encoding="utf-8") as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=["Filename"] + headers_to_extract)
     
     writer.writeheader()
 
+    
     for root, dirs, files in os.walk(directory_path):
         for filename in files:
             if filename.startswith("."):
@@ -22,19 +24,25 @@ with open(csv_file_path, mode="w", newline="", encoding="utf-8") as csvfile:
             filepath = os.path.join(root, filename)
 
             try:
-                with open(filepath, "r", encoding="utf-8", errors="ignore") as eml_file:
-                    msg = email.message_from_file(eml_file, policy=policy.default)
+                
+                if filename.endswith('.eml'):
+                    with open(filepath, "r", encoding="utf-8", errors="ignore") as eml_file:
+                        msg = email.message_from_file(eml_file, policy=policy.default)
 
-             
-                row = {"Filename": filename}  
-                for header in headers_to_extract:
-                    row[header] = msg.get(header, "")
+                    row = {"Filename": filename}  
+                    for header in headers_to_extract:
+                        row[header] = msg.get(header, "")
 
-           
-                writer.writerow(row)
+                    writer.writerow(row)
+                
+                
+                elif filename == 'phishing3.mbox':
+                    mbox = mailbox.mbox(filepath)
+                    for i, msg in enumerate(mbox):
+                        row = {"Filename": f"phishing3_msg_{i+1}"}
+                        for header in headers_to_extract:
+                            row[header] = msg.get(header, "")
+                        writer.writerow(row)
 
             except Exception as e:
                 print(f"Error reading {filepath}: {e}")
-
-
-
